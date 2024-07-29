46effa7efae6a0a7ee8c78a0e906d0f8a1e3e385
ZeroOne <71119629+ZeroOneDeveloper@users.noreply.github.com>
2024-07-29 17:13:41 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "상품표시이력제",
+  title: "제품표시이력제",
   description: "Created with blockchain tech.",
 };
 
